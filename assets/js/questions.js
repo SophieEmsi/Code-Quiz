ee2dfe323e6f46c67190c3ac7ce52f44142de544
Sophie McNally <soph_mcnally@msn.com>
2023-04-01 15:52:15 +0100

    adding questions to js

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [ 
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: 2
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: 1
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: 3
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: 3
+    },
+    ];
