cba03a2a5b22407bcde1d6c34d70fb31c1304b12
Sophie McNally <soph_mcnally@msn.com>
2023-04-02 13:41:48 +0100

    getting the console log to show if answer is correct or incorrect

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,7 +9,7 @@ function startQuiz() {
 
 
 firstQuestion(0); // to call the first question in the list starting at index 0.
-
+}
 function firstQuestion(index) { // creates function with index as its parameter to display the questions and choices
     var questionTitle = document.getElementById("question-title"); // takes the emelement "question-title" from the index.html page and uses it to create a variable called questionTitle.
     var questionChoices = document.getElementById("choices");// takes the emelement "choices" from the index.html page and uses it to create a variable called questionChoices.
@@ -29,19 +29,35 @@ questionChoices.innerHTML = ""; //this will clear the choices element after maki
  
     for (var i = 0; i < questions[index].choices.length; i++){
     var choiceButton = document.createElement("button");
- 
+    
+// Everything seemed to be in the wrong order so i moved this. I haven't told the code what I want the buttons to say so I need to create a line that takes the index choice from the choices array in questions.js and creates a button for each choice.
 
-// I haven't told the code what I want the buttons to say so I need to create a line that takes the index choice from the choices array in questions.js and creates a button for each choice.
+choiceButton.textContent = questions[index].choices[i];
 
-  choiceButton.textContent = questions[index].choices[i];
+        
+
+// add event listener to the choice button so that when it is clicked the event of checking the answer is triggered. eg. choiceButton.addEventListener(click, event); if event text target matches the answer from questions.js then show correct! as a message and if the answer does not match then else and show incorrect! as message. In this example we are checking to see if the text of the clicked button matched the correctAnswer var which gets its data from the answer. If correct we log correct and otherwise we log incorrect. I will try this in the console to begin with before adding message to browser.
+ choiceButton.addEventListener("click", function(event) {
+    var answer = questions[index].answer; // create var to display the correct answer taken from questions.js.
+    if (event.target.textContent === answer) {
+        console.log("correct!");
+    } else {
+        console.log("incorrect!");
+    }
+    });
+
+    
+
+
 
   //i've created the buttons for each choice and the loop to go through each question and its choices so I created an event listener to the choiceButton otherwise nothing will happen with it is clicked.
 
-  choiceButton.addEventListener("click", function() {});
+//   choiceButton.addEventListener("click", function() {}); duplicated line of code.
 
   //i'm still not getting the choices show up on the browser but i forgot that the choiceButton needs to be added to the questionChoices element as a child. Apppendchild will add the choiceButton to the questionChoices which stores the html element of id choices so this links the button back to the html.
 
-  questionChoices.appendChild(choiceButton);
+     questionChoices.appendChild(choiceButton);
 
-} }
+  // I need the choiceButton event to tell the player if the answer is correct or not. To do this i will modify the choiceButton with if else.
+    } 
 }
